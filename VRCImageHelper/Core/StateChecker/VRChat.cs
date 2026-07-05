17376b5baf347226cac9f876f3a6fec6a39c6632
m-hayabusa <s.hashimoto@hs-sh.net>
2026-07-05 20:11:10 +0900

    wrldIDの抽出を調整

--- VRCImageHelper/Core/StateChecker/VRChat.cs
+++ VRCImageHelper/Core/StateChecker/VRChat.cs
@@ -7,7 +7,7 @@ internal static class VRChat
 {
     public static void WorldId(object sender, NewLineEventArgs e)
     {
-        var match = Regex.Match(e.Line, @"(?<DateTime>\d+\.\d+\.\d+ \d+:\d+:\d+) .*\[Behaviour\] Joining (?<WorldID>wrld_.*?):(?<InstanceID>.*?)~(?<Options>.*)?");
+        var match = Regex.Match(e.Line, @"(?<DateTime>\d+\.\d+\.\d+ \d+:\d+:\d+) .*\[Behaviour\] Joining (?<WorldID>wrld_[^:]+):(?<InstanceID>[^~]*)(?:~(?<Options>.*))?$");
         if (match.Success)
         {
             Debug.WriteLine($"Joining {match.Groups["WorldID"]}, {match.Groups["InstanceID"]}, {match.Groups["Options"]}");
